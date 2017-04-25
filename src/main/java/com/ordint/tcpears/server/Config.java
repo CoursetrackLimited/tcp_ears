c88d9def2b8ed2a50a2ec45007b7afd1fb7960da
Tom Lyle <tlyle@adhslx.com>
2017-04-25 10:47:08 +0100

    More proguard config

--- src/main/java/com/ordint/tcpears/server/Config.java
+++ src/main/java/com/ordint/tcpears/server/Config.java
@@ -2,7 +2,6 @@ package com.ordint.tcpears.server;
 
 import java.beans.PropertyVetoException;
 import java.io.File;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,27 +41,17 @@ import com.ordint.tcpears.service.replay.MySqlReplayService;
 import io.netty.bootstrap.AbstractBootstrap;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.DefaultEventLoopGroup;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.epoll.EpollChannelOption;
 import io.netty.channel.epoll.EpollDatagramChannel;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.util.CharsetUtil;
 @Configuration
 @ComponentScan("com.ordint.tcpears")
 @PropertySource("classpath:netty.properties")
@@ -73,7 +62,6 @@ public class Config {
 	@Value("${boss.thread.count}")
 	private int bossCount;
 
-;
 	@Value("${admin.port}")
 	private int adminPort;	
 
